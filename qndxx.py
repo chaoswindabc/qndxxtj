d3acacbfd68bbfe15122b7e324a688cf74d2b5c0
chaoswinder <1044865590@qq.com>
2022-05-20 13:43:15 +0800

    update

--- qndxx.py
+++ qndxx.py
@@ -2,17 +2,19 @@ from openpyxl import load_workbook
 
 
 class cl:
-    name=''
-    total=0  #总计
-    unfi=0  #未完成
-    per=0  #完成率
+    grade = 0
+    name = ''
+    total = 0  #总计
+    unfi = 0  #未完成
+    per = 0  #完成率
 
     def __init__(self,n,t):
         self.name = n
         self.total = t
 
     def cal(self):
-        self.per=1-(self.unfi/self.total)
+        self.per = 1 - (self.unfi/self.total)
+        return self.per
 
 
 def cnt(nam):
@@ -28,12 +30,12 @@ wb3 = load_workbook("总人数表.xlsx")
 sh1 = wb1["Sheet1"]
 sh2 = wb2["Sheet1"]
 sh3 = wb3["Sheet1"]
-total_stu=sh1.max_row
+# total_stu=sh1.max_row
 # print(total_stu)
 
 arr=[]
 for i in range(1,sh3.max_row+1):
-    arr.append(cl(sh3.cell(i,1).value,sh3.cell(i,2).value))
+    arr.append(cl(sh3.cell(i,1).value, sh3.cell(i,2).value))
 
 # for i in range(3,total_stu):
 #     cell = sh1.cell(i, 2)
@@ -47,4 +49,4 @@ for col in sh1.rows:
 
 for i in range(0,sh3.max_row):
     arr[i].cal()
-    print(arr[i].name,arr[i].total,arr[i].unfi,arr[i].per)
+    print(arr[i].name, arr[i].per)
